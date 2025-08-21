e83188b0ada1782e85014140d714cf1239142abd
mooon-elxna <kreinelina@gmail.com>
2025-08-21 12:41:26 +0200

    learnt about oninput w copilot and implementend it

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function convert(){
+
+function convertToM(){
+
+    const cm = Number(document.getElementById("cm").value)
+    let result =  cm /100
+    console.log(result)
+    document.getElementById("m").value = result;
+
+}
+
+
+function convertToCm(){
+    const m = Number(document.getElementById("m").value)
+    let result = m*100
+    console.log(result)
+    document.getElementById("cm").value = result; 
+
+}
+
+/* function convert(){
     const cm = Number(document.getElementById("cm").value)
     const m = Number(document.getElementById("m").value)
 
@@ -17,4 +36,6 @@ function convert(){
         alert("Error!")
     }
 
-}
+}*/
+
+//this function was a part of the first version, where i used a button in the html
