8643aaeb0f23f9b1ae87ffc92be7d1b40132c1f6
mooon-elxna <kreinelina@gmail.com>
2025-08-23 14:15:51 +0200

    edit

--- script.js
+++ script.js
@@ -36,24 +36,3 @@ function setLang(){
     }
 
 }
-
-/* function convert(){
-    const cm = Number(document.getElementById("cm").value)
-    const m = Number(document.getElementById("m").value)
-
-    if (cm != 0){
-       let result =  cm /100
-       document.getElementById("m").value = result;
-    }
-    else if (m != 0){
-        let result = m*100
-        document.getElementById("cm").value = result;
-    }
-
-    else{
-        alert("Error!")
-    }
-
-}*/
-
-//this function was a part of the first version, where i used a button in the html 
